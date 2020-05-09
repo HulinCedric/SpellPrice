a0b0f7276eec97b91397e062637f4925e37d6130
Cédric Hulin <hulin.cedric@gmail.com>
2020-05-09 16:53:13 +0200

    refactor: Extract natural part price into class

---
+++ src/SpellPrice/NaturalPartPrice.cs
@@ -0,0 +1,39 @@
+﻿using Humanizer;
+using System;
+using System.Globalization;
+
+namespace SpellPrice
+{
+    internal class NaturalPartPrice
+    {
+        private readonly CultureInfo cultureInfo;
+        private readonly RegionInfo regionInfo;
+
+        public NaturalPartPrice(CultureInfo cultureInfo)
+        {
+            this.cultureInfo = cultureInfo ?? throw new ArgumentNullException(nameof(cultureInfo));
+            this.regionInfo = new RegionInfo(cultureInfo.LCID);
+        }
+
+        public string GetPriceToWords(decimal price)
+        {
+            var naturalPartPrice = (int)Math.Truncate(price);
+            var naturalPartPriceToWords = naturalPartPrice.ToWords(cultureInfo);
+
+            var currencyName = GetCurrencyName(price, regionInfo);
+
+            return $"{naturalPartPriceToWords} {currencyName}";
+        }
+
+        private static string GetCurrencyName(decimal price, RegionInfo regionInfo)
+        {
+            var currencyName = regionInfo.CurrencyNativeName;
+            if (price > 1m)
+            {
+                currencyName = currencyName.Pluralize();
+            }
+
+            return currencyName;
+        }
+    }
+}
--- src/SpellPrice/PriceExtensions.cs
+++ src/SpellPrice/PriceExtensions.cs
@@ -1,5 +1,4 @@
 using Humanizer;
-using System;
 using System.Globalization;
 
 namespace SpellPrice
@@ -8,37 +7,9 @@ namespace SpellPrice
     {
         public static string PriceToWords(this decimal price, CultureInfo cultureInfo)
         {
-            var naturalPartPriceToWords = GetNaturalPartPrice(price, cultureInfo);
+            var naturalPartPriceToWords = new NaturalPartPrice(cultureInfo).GetPriceToWords(price);
 
             return $"{naturalPartPriceToWords}".Humanize(LetterCasing.Sentence);
-        }
-
-        private static string GetCurrencyName(decimal price, RegionInfo regionInfo)
-        {
-            var currencyName = regionInfo.CurrencyNativeName;
-            if (price > 1m)
-            {
-                currencyName = currencyName.Pluralize();
-            }
-
-            return currencyName;
-        }
-
-        private static string GetNaturalPartPrice(this decimal price, CultureInfo cultureInfo)
-        {
-            if (cultureInfo == null)
-            {
-                throw new ArgumentNullException(nameof(cultureInfo));
-            }
-
-            var regionInfo = new RegionInfo(cultureInfo.LCID);
-
-            var naturalPartPrice = (int)Math.Truncate(price);
-            var naturalPartPriceToWords = naturalPartPrice.ToWords(cultureInfo);
-
-            var currencyName = GetCurrencyName(price, regionInfo);
-
-            return $"{naturalPartPriceToWords} {currencyName}";
         }
     }
 }
