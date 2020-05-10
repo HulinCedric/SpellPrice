5a4eff6e4447173edae5bade1be0a3d03471e793
Cédric Hulin <hulin.cedric@gmail.com>
2020-05-10 16:41:48 +0200

    refactor: Remove redundant assignment

--- src/SpellPrice/PriceExtensions.cs
+++ src/SpellPrice/PriceExtensions.cs
@@ -7,10 +7,10 @@ namespace SpellPrice
     {
         public static string PriceToWords(this decimal priceValue, CultureInfo cultureInfo)
         {
-            var priceToWords = string.Empty;
             var price = new Price(priceValue);
             var integerPartPriceToWords = new IntegerPartPrice(cultureInfo).GetPriceToWords(price);
 
+            string priceToWords;
             if (price.FractionalPartValue != 0)
             {
                 var fractionalPartPriceToWords = new FractionalPartPrice(cultureInfo).GetPriceToWords(price);
