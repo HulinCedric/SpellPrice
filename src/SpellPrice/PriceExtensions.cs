a0dc7274e4a756b49e6d17f3eefe6c7c1cacc323
Cédric Hulin <hulin.cedric@gmail.com>
2020-05-10 16:51:39 +0200

    refactor: Extract format into price formatter

--- src/SpellPrice/PriceExtensions.cs
+++ src/SpellPrice/PriceExtensions.cs
@@ -8,20 +8,9 @@ namespace SpellPrice
         public static string PriceToWords(this decimal priceValue, CultureInfo cultureInfo)
         {
             var price = new Price(priceValue);
-            var integerPartPriceToWords = new IntegerPartPrice(cultureInfo).GetPriceToWords(price);
+            var priceFormatter = new PriceFormatter(cultureInfo);
 
-            string priceToWords;
-            if (price.FractionalPartValue != 0)
-            {
-                var fractionalPartPriceToWords = new FractionalPartPrice(cultureInfo).GetPriceToWords(price);
-                priceToWords = $"{integerPartPriceToWords} et {fractionalPartPriceToWords}";
-            }
-            else
-            {
-                priceToWords = $"{integerPartPriceToWords}";
-            }
-
-            return priceToWords.Humanize(LetterCasing.Sentence);
+            return priceFormatter.GetPriceToWords(price).Humanize(LetterCasing.Sentence);
         }
     }
 }
---
+++ src/SpellPrice/PriceFormatter.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace SpellPrice
+{
+    internal class PriceFormatter
+    {
+        private readonly CultureInfo cultureInfo;
+
+        public PriceFormatter(CultureInfo cultureInfo)
+            => this.cultureInfo = cultureInfo;
+
+        public string GetPriceToWords(Price price)
+        {
+            var integerPartPriceToWords = new IntegerPartPrice(cultureInfo).GetPriceToWords(price);
+
+            string priceToWords;
+            if (price.FractionalPartValue != 0)
+            {
+                var fractionalPartPriceToWords = new FractionalPartPrice(cultureInfo).GetPriceToWords(price);
+                priceToWords = $"{integerPartPriceToWords} et {fractionalPartPriceToWords}";
+            }
+            else
+            {
+                priceToWords = $"{integerPartPriceToWords}";
+            }
+
+            return priceToWords;
+        }
+    }
+}
